571f11af49335c7bc4ceb3cccd0ce48fc75c8221
Theo Wimber <108815460+theodll@users.noreply.github.com>
2026-01-25 17:49:43 +0100

    feat(uniform): add uniform buffer files

---
+++ Velt/src/Velt/Platform/Vulkan/Buffer/VulkanUniformBuffer.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Renderer/UniformBuffer.h"
+
+namespace Velt::Renderer::Vulkan
+{
+    class UniformBuffer : public Renderer::UniformBuffer 
+    {
+        public:
+
+    }
+}
---
+++ Velt/src/Velt/Renderer/UniformBuffer.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "Velt/Core/Core.h"
+#include "Buffer.h"
+
+namespace Velt::Renderer
+{
+
+    struct VertexBufferElement
+    {
+        void* data;
+        u32 Size;
+    };
+
+    struct Vertex
+    {
+        glm::vec2 position;
+        glm::vec3 color;
+    };
+
+
+    class UniformBuffer
+    {
+    public:
+        virtual ~UniformBuffer() {};
+
+		virtual void SetData(void* data, u64 size, u64 offset = 0) = 0;
+        
+        virtual void Upload(VkCommandBuffer commandBuffer) = 0;
+		
+        virtual VkBuffer GetVulkanBuffer() const = 0;
+        
+		static std::shared_ptr<UniformBuffer> Create(void* uniformData, u64 size, u64 offset);
+    };
+} 
