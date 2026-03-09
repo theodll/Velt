5e1a0895d37096d3c8231c390bff442c6ab47b5b
theodll <theowimber@gmail.com>
2026-03-09 17:03:45 +0100

    feat(shader): start working on reflection

--- Velt/src/Velt/Renderer/ShaderLibrary.cpp
+++ Velt/src/Velt/Renderer/ShaderLibrary.cpp
@@ -1,9 +1,11 @@
 #include "ShaderLibrary.h"
 #include "Core/Core.h"
+#include "spirv_reflect.h"
+#include <filesystem>
 
 namespace Velt 
 {
-	std::unordered_map <std::filesystem::path, Ref<Shader >> ShaderLibrary::m_Shaders{};
+	std::unordered_map <std::filesystem::path, Ref<Shader>> ShaderLibrary::m_Shaders{};
 	Ref<Shader> ShaderLibrary::Get(const std::filesystem::path& path)
 	{
 		auto it = m_Shaders.find(path);
@@ -35,11 +37,122 @@ namespace Velt
 
 	VkShaderModule ShaderLibrary::CreateVulkanModule(const std::vector<u32>& bytes)
 	{
-		return VT_NULL_HANDLE;
+		VkShaderModule module; 
+
+		VkShaderModuleCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+		createInfo.pNext = VT_NULL_HANDLE;
+		createInfo.flags = 0;
+		createInfo.codeSize = bytes.size();
+		createInfo.pCode = reinterpret_cast<const uint32_t*>(bytes.data());
+
+		VT_VK_CHECK(
+			vkCreateShaderModule(VulkanContext::GetDevice()->device(), &createInfo, VT_NULL_HANDLE, module),
+			"Failed to create shader module!"
+		);
+
+		return module;
+	}
+	
+	static RHI::DescriptorType FromSpvReflectDescriptorType(SpvReflectDescriptorType type)
+	{
+		switch (type)
+		{
+		case SPV_REFLECT_DESCRIPTOR_TYPE_SAMPLER:
+			return RHI::DescriptorType::SAMPLER;
+
+		case SPV_REFLECT_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER:
+			return RHI::DescriptorType::COMBINED_IMAGE_SAMPLER;
+
+		case SPV_REFLECT_DESCRIPTOR_TYPE_SAMPLED_IMAGE:
+			return RHI::DescriptorType::SAMPLED_IMAGE;
+
+		case SPV_REFLECT_DESCRIPTOR_TYPE_STORAGE_IMAGE:
+			return RHI::DescriptorType::STORAGE_IMAGE;
+
+		case SPV_REFLECT_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER:
+			return RHI::DescriptorType::UNIFORM_TEXEL_BUFFER;
+
+		case SPV_REFLECT_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER:
+			return RHI::DescriptorType::STORAGE_TEXEL_BUFFER;
+
+		case SPV_REFLECT_DESCRIPTOR_TYPE_UNIFORM_BUFFER:
+			return RHI::DescriptorType::UNIFORM_BUFFER;
+
+		case SPV_REFLECT_DESCRIPTOR_TYPE_STORAGE_BUFFER:
+			return RHI::DescriptorType::STORAGE_BUFFER;
+
+		case SPV_REFLECT_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC:
+			return RHI::DescriptorType::UNIFORM_BUFFER_DYNAMIC;
+
+		case SPV_REFLECT_DESCRIPTOR_TYPE_STORAGE_BUFFER_DYNAMIC:
+			return RHI::DescriptorType::STORAGE_BUFFER_DYNAMIC;
+
+		case SPV_REFLECT_DESCRIPTOR_TYPE_INPUT_ATTACHMENT:
+			return RHI::DescriptorType::INPUT_ATTACHMENT;
+
+		default:
+			VT_CORE_ASSERT(false, "Unsupported descriptor type from SPIRV-Reflect");
+			return RHI::DescriptorType{};
+		}
 	}
 
-	DescriptorSetLayoutData ReflectShader(const std::vector<u32>& bytes)
+	static RHI::ShaderStage FromSpvShaderStage(SpvReflectShaderStageFlagBits stage) 
 	{
-		return {};
+		switch (stage) 
+		{
+			case	SPV_REFLECT_SHADER_STAGE_VERTEX_BIT:
+				return RHI::ShaderStage::VERTEX;
+			case	SPV_REFLECT_SHADER_STAGE_FRAGMENT_BIT:
+				return RHI::ShaderStage::FRAGMENT;
+			default: 
+				VT_CORE_ASSERT(false, "Unsupported Stage Flag from SPIRV-Reflect");
+				return RHI::ShaderStage{};
+		}
+	}
+
+	std::vector<DescriptorSetLayoutData> ReflectShader(const std::vector<u32>& bytes)
+	{
+		SpvReflectShaderModule module{};
+		
+		SpvReflectResult result = spvReflectCreateShaderModule(bytes.size() * sizeof(u32), bytes.data(), &module); 
+		VT_CORE_ASSERT(result == SPV_REFLECT_RESULT_SUCCESS, "Failed to create SpvReflectShaderModule");
+
+		u32 count{};
+		result = spvReflectEnumerateDescriptorBindings(&module, &count, VT_NULL_HANDLE); 
+		VT_CORE_ASSERT(result == SPV_REFLECT_RESULT_SUCCESS, "Failed to enumerate Descriptor Sets in Shader Module");
+
+		std::vector<SpvReflectDescriptorSet*> sets(count);
+  		result = spvReflectEnumerateDescriptorSets(&module, &count, sets.data());
+		VT_CORE_ASSERT(result == SPV_REFLECT_RESULT_SUCCESS, "Failed to enumerate Descriptor Sets in Shader Module");
+
+		std::vector<DescriptorSetLayoutData> set_layouts(sets.size(), DescriptorSetLayoutData{});
+
+		// (c) Khronos Group - Spirv Reflect - https://github.com/KhronosGroup/SPIRV-Reflect/blob/main/examples/main_descriptors.cpp
+		// slightly modified
+
+		for (size_t i_set = 0; i_set < sets.size(); ++i_set) {
+
+			const SpvReflectDescriptorSet& reflectionSet = *(sets[i_set]);
+			DescriptorSetLayoutData& layout = set_layouts[i_set];
+
+			layout.Bindings.resize(reflectionSet.binding_count);
+			for (uint32_t i_binding = 0; i_binding < reflectionSet.binding_count; ++i_binding) {
+			const SpvReflectDescriptorBinding& refl_binding = *(reflectionSet.bindings[i_binding]);
+			RHI::DescriptorBinding& layout_binding = layout.Bindings[i_binding];
+			layout_binding.binding = refl_binding.binding;
+			layout_binding.type = FromSpvReflectDescriptorType(refl_binding.descriptor_type);
+			layout_binding.count = 1;
+			for (uint32_t i_dim = 0; i_dim < refl_binding.array.dims_count; ++i_dim) {
+				layout_binding.count *= refl_binding.array.dims[i_dim];
+			}
+			layout_binding.stage = FromSpvShaderStage(module.shader_stage);
+			}
+			layout.SetNumber = refl_set.set;
+			// layout.create_info.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+			//layout.create_info.bindingCount = refl_set.binding_count;
+			//layout.create_info.pBindings = layout.bindings.data();
+		}
+
 	}
 }
