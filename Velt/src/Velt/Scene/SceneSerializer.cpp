eed6bb74f197c3b96ae75d5090afc54dfcdd1c8a
theodll <theowimber@gmail.com>
2026-04-19 16:11:25 +0200

    feat(scene-serialization): add deserialization

--- Velt/src/Velt/Scene/SceneSerializer.cpp
+++ Velt/src/Velt/Scene/SceneSerializer.cpp
@@ -6,8 +6,108 @@
 
 #include <yaml-cpp/yaml.h>
 
+namespace YAML {
+
+	template<>
+	struct convert<glm::vec2>
+	{
+		static Node encode(const glm::vec2& rhs)
+		{
+			Node node;
+			node.push_back(rhs.x);
+			node.push_back(rhs.y);
+			node.SetStyle(EmitterStyle::Flow);
+			return node;
+		}
+
+		static bool decode(const Node& node, glm::vec2& rhs)
+		{
+			if (!node.IsSequence() || node.size() != 2)
+				return false;
+
+			rhs.x = node[0].as<float>();
+			rhs.y = node[1].as<float>();
+			return true;
+		}
+	};
+
+	template<>
+	struct convert<glm::vec3>
+	{
+		static Node encode(const glm::vec3& rhs)
+		{
+			Node node;
+			node.push_back(rhs.x);
+			node.push_back(rhs.y);
+			node.push_back(rhs.z);
+			node.SetStyle(EmitterStyle::Flow);
+			return node;
+		}
+
+		static bool decode(const Node& node, glm::vec3& rhs)
+		{
+			if (!node.IsSequence() || node.size() != 3)
+				return false;
+
+			rhs.x = node[0].as<float>();
+			rhs.y = node[1].as<float>();
+			rhs.z = node[2].as<float>();
+			return true;
+		}
+	};
+
+	template<>
+	struct convert<glm::vec4>
+	{
+		static Node encode(const glm::vec4& rhs)
+		{
+			Node node;
+			node.push_back(rhs.x);
+			node.push_back(rhs.y);
+			node.push_back(rhs.z);
+			node.push_back(rhs.w);
+			node.SetStyle(EmitterStyle::Flow);
+			return node;
+		}
+
+		static bool decode(const Node& node, glm::vec4& rhs)
+		{
+			if (!node.IsSequence() || node.size() != 4)
+				return false;
+
+			rhs.x = node[0].as<float>();
+			rhs.y = node[1].as<float>();
+			rhs.z = node[2].as<float>();
+			rhs.w = node[3].as<float>();
+			return true;
+		}
+	};
+
+}
+
 namespace Velt   
 {
+
+	YAML::Emitter& operator<<(YAML::Emitter& out, const Vector& v)
+	{
+		out << YAML::Flow;
+		out << YAML::BeginSeq << v.x << v.y << v.x << YAML::EndSeq;
+		return out; 
+	}
+
+	YAML::Emitter& operator<<(YAML::Emitter& out, const HVector& v)
+	{
+		out << YAML::Flow;
+		out << YAML::BeginSeq << v.x << v.y << v.x << v.w << YAML::EndSeq;
+		return out;
+	}
+
+	YAML::Emitter& operator<<(YAML::Emitter& out, const Quaternion& q)
+	{
+		out << YAML::Flow;
+		out << YAML::BeginSeq << q.x << q.y << q.x << q.w << YAML::EndSeq;
+		return out;
+	}
 
 	SceneSerializer::SceneSerializer(const Ref<Scene>& scene) : m_Scene(scene)
 	{
@@ -16,14 +116,14 @@ namespace Velt
 	}
 
 
-	static void SerializeEntity(YAML::Emitter& out, Entity ent) 
+	static void SerializeEntity(YAML::Emitter& out, Entity ent)
 	{
 		VT_PROFILE_FUNCTION();
+		
 		out << YAML::BeginMap;
-		out << YAML::Key << "Entity" << YAML::Value << "2093482093840";
-		out << YAML::EndMap;
+		out << YAML::Key << "Entity" << YAML::Value << "2093482093840"; 
 
-		if (ent.HasComponent<TagComponent>()) 
+		if (ent.HasComponent<TagComponent>())
 		{
 			out << YAML::Key << "TagComponent";
 			out << YAML::BeginMap;
@@ -31,7 +131,7 @@ namespace Velt
 			const auto& tag = ent.GetComponent<TagComponent>().Tag;
 			out << YAML::Key << "Tag" << YAML::Value << tag;
 
-			out << YAML::EndMap;
+			out << YAML::EndMap; 
 		}
 
 		if (ent.HasComponent<TransformComponent>())
@@ -41,36 +141,12 @@ namespace Velt
 
 			const auto& transform = ent.GetComponent<TransformComponent>();
 
-			out << YAML::Key << "Translation";
-			out << YAML::BeginMap;
-			out << YAML::Key << "X" << YAML::Value << transform.Translation.x;
-			out << YAML::Key << "Y" << YAML::Value << transform.Translation.y;
-			out << YAML::Key << "Z" << YAML::Value << transform.Translation.z;
-			out << YAML::EndMap;
-			
-			out << YAML::Key << "Scale";
-			out << YAML::BeginMap;
-			out << YAML::Key << "X" << YAML::Value << transform.Scale.x;
-			out << YAML::Key << "Y" << YAML::Value << transform.Scale.y;
-			out << YAML::Key << "Z" << YAML::Value << transform.Scale.z;
-			out << YAML::EndMap;
+			out << YAML::Key << "Translation" << YAML::Value << transform.Translation;
+			out << YAML::Key << "Rotation" << YAML::Value << transform.EulerDegrees;
+			out << YAML::Key << "Scale" << YAML::Value << transform.Scale;
+			out << YAML::Key << "Quaternion" << YAML::Value << transform.Rotation;
 
-			out << YAML::Key << "Rotation (Euler)";
-			out << YAML::BeginMap;
-			out << YAML::Key << "X" << YAML::Value << transform.EulerDegrees.x;
-			out << YAML::Key << "Y" << YAML::Value << transform.EulerDegrees.y;
-			out << YAML::Key << "Z" << YAML::Value << transform.EulerDegrees.z;
-			out << YAML::EndMap;
-
-			out << YAML::Key << "Rotation (Quaternion)";
-			out << YAML::BeginMap;
-			out << YAML::Key << "X" << YAML::Value << transform.Rotation.x;
-			out << YAML::Key << "Y" << YAML::Value << transform.Rotation.y;
-			out << YAML::Key << "Z" << YAML::Value << transform.Rotation.z;
-			out << YAML::Key << "W" << YAML::Value << transform.Rotation.w;
-			out << YAML::EndMap;
-
-			out << YAML::EndMap;
+			out << YAML::EndMap; 
 		}
 
 		if (ent.HasComponent<ModelComponent>())
@@ -79,16 +155,12 @@ namespace Velt
 			out << YAML::BeginMap;
 
 			const auto& model = ent.GetComponent<ModelComponent>();
+			out << YAML::Key << "MeshPath" << YAML::Value << model.Path.string();
 
-			out << YAML::Key << "Model";
-			out << YAML::BeginMap;
-			out << YAML::Key << "Mesh Path" << YAML::Value << model.Path.string();;
-			out << YAML::EndMap;
-
-
-			out << YAML::EndMap;
+			out << YAML::EndMap; 
 		}
 
+		out << YAML::EndMap;
 	}
 
 	void SceneSerializer::SerializeText(const std::filesystem::path& path)
@@ -127,7 +199,58 @@ namespace Velt
 
 	void SceneSerializer::DeserializeText(const std::filesystem::path& path)
 	{
+		VT_PROFILE_FUNCTION();
 
+		std::ifstream fin(path);
+		std::stringstream strStream;
+		strStream << fin.rdbuf();
+
+		YAML::Node data = YAML::Load(strStream.str()); 
+		if (!data["Scene"])
+			return;
+
+		std::string sceneName = data["Scene"].as<std::string>();
+		VT_CORE_INFO("Deserializing Scene {}", sceneName);
+
+		auto entities = data["Entities"];
+		if(entities)
+		{
+			for (auto entity : entities)
+			{
+				u64 uuid = entity["Entity"].as<u64>();
+
+				std::string name;
+				auto tagComponent = entity["TagComponent"];
+				if (tagComponent)
+					name = tagComponent["Tag"].as<std::string>();
+
+				VT_CORE_INFO("Deserializing Entity {0} - {1}", uuid, name);
+
+				Entity deserializedEntity = m_Scene->CreateEntity(name);
+
+				auto transformComponent = entity["TransformComponent"];
+				if (transformComponent)
+				{
+					auto& tc = deserializedEntity.GetComponent<TransformComponent>();
+					tc.Translation = transformComponent["Translation"].as<Vector>();
+					tc.Scale = transformComponent["Scale"].as<Vector>();
+					tc.EulerDegrees = transformComponent["Rotation"].as<Vector>();
+					tc.Rotation = transformComponent["Quaternion"].as<Quaternion>();
+				}
+
+				auto modelComponent = entity["ModelComponent"];
+				if (transformComponent)
+				{
+					ModelComponent mc;
+					if (deserializedEntity.HasComponent<ModelComponent>())
+						mc = deserializedEntity.GetComponent<ModelComponent>();
+					else
+						mc = deserializedEntity.AddComponent<ModelComponent>();
+
+					mc.Path = transformComponent["MeshPath"].as<std::filesystem::path>();
+				}
+			}
+		}
 	}
 
 	void SceneSerializer::DeserializeBinary(const std::filesystem::path& path)
